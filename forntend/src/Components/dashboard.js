074982eaf6913f8171b98ab7f78a921dfd11809e
shantanu994 <shantanukulkarni994@gmail.com>
2026-02-07 01:49:10 +0530

    Changes

--- forntend/src/Components/dashboard.js
+++ forntend/src/Components/dashboard.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Dashboard = (props) => {
-    return (
-        <div className='dashboard'>
-            <h1>{props.title}</h1>
-
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <h1>{props.title}</h1>
+      <div>
+        <form className="row g-3">
+          <div className="col-md-6">
+            <label htmlFor="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" id="inputEmail4" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputPassword4" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword4"
+            />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="inputState" className="form-label">
+              Gender
+            </label>
+            <select id="inputState" className="form-select">
+              <option selected>choose...</option>
+              <option>Male</option>
+              <option>Female</option>
+              <option>Other</option>
+            </select>
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
