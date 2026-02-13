5f2a05e496ff41bab83adb4e4fad36d1b2539cd8
shantanu994 <shantanukulkarni994@gmail.com>
2026-02-13 11:19:33 +0530

    first

---
+++ forntend/src/pages/first.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+import React from 'react';
+
+function first(){
+    return(
+        <>
+        <div className='first'>
+            <h4>hello</h4>
+        </div>
+        </>
+    )
+}
+
+export default first;
