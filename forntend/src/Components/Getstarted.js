6dcf40d3a089c09fbefc2d2b1f50a1df25efb8fb
shantanu994 <shantanukulkarni994@gmail.com>
2026-02-13 11:19:33 +0530

    first

---
+++ forntend/src/Components/Getstarted.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './getstarted.css';
+
+function Getstarted(){
+    return( 
+        <div className='getstarted'>
+            <h1>Get Started</h1>
+        </div>
+    )
+}
+
+export default Getstarted;
