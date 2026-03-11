c0f0e860aa20189b36287926daf7f1f2bc08908c
shantanu994 <shantanukulkarni994@gmail.com>
2026-03-11 12:47:02 +0530

    css changes

---
+++ backend/db.py
@@ -0,0 +1,37 @@
+import pymysql
+from pymysql.cursors import DictCursor
+
+
+# Database configuration
+DB_CONFIG = {
+    "host": "localhost",      # MySQL server location
+    "user": "root",           # MySQL username
+    "password": "",           # your MySQL password (leave empty if none)
+    "database": "attendance_db",   # database name you created
+    "charset": "utf8mb4",
+    "cursorclass": DictCursor
+}
+
+
+def get_connection():
+    """
+    This function creates a connection to MySQL.
+    Whenever we want to run a query (SELECT, INSERT, UPDATE),
+    we call this function.
+    """
+
+    connection = pymysql.connect(
+        host=DB_CONFIG["host"],
+        user=DB_CONFIG["user"],
+        password=DB_CONFIG["password"],
+        database=DB_CONFIG["database"],
+        charset=DB_CONFIG["charset"],
+        cursorclass=DB_CONFIG["cursorclass"]
+    )
+
+    return connection
+
+if __name__ == "__main__":
+    conn = get_connection()
+    print("Database Connected Successfully!")
+    conn.close()
